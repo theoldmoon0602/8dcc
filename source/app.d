24438e10dd9e3650f318e9d4eb4fc2cdca0cf28c
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-07-06 22:02:48 +0900

    Introduce AST.

--- source/app.d
+++ source/app.d
@@ -7,6 +7,38 @@ import core.stdc.stdlib;
 import core.stdc.stdio : ungetc;
 
 
+enum AST_TYPE {
+	OP_PLUS,
+	OP_MINUS,
+	INT,
+	STR
+}
+
+class Ast {
+	AST_TYPE type;
+	union {
+		int ival;
+		string sval;
+		struct {
+			Ast left;
+			Ast right;
+		}
+	}
+	this(AST_TYPE type, Ast left, Ast right) {
+		this.type = type;
+		this.left = left;
+		this.right = right;
+	}
+
+	this(int val) {
+		this.type = AST_TYPE.INT;
+		this.ival = val;
+	}
+	this(string str) {
+		this.type = AST_TYPE.STR;
+		this.sval = str;
+	}
+}
 void skip_space() {
 	dchar c;
 	while (readf("%c", c)) {
@@ -14,61 +46,64 @@ void skip_space() {
 			continue;
 		}
 		ungetc(c.to!char, stdin.getFP);
-		return;
+		break;
 	}
+	return;
 }
 
-int read_number(int n) {
-	dchar c;
-	while (readf("%c", c)) {
+Ast read_number(int n) {
+	while (true) {
+		dchar c;
+		readf("%c", c);
 		if (!c.isNumber) {
 			ungetc(c.to!char, stdin.getFP);
 			break;
 		}
 		n = n * 10 + cast(int)(c-'0');
 	}
-	return n;
+	return new Ast(n);
 }
 
-void compile_expr2() {
-	while (true) {
-		skip_space();
-		dchar c;
-		if (!readf("%c", c)) {
-			writeln("ret");
-			exit(0);
-		}
-		string op;
-		if (c == '+') {
-			op = "add";
-		}
-		else if (c == '-') {
-			op = "sub";
-		}
-		else {
-			stderr.writef("Operator expected, but got '%c'", c);
-			exit(1);
-		}
-		skip_space();
-		readf("%c", c);
-		if (!c.isNumber) {
-			stderr.writef("Number expected, but got '%c'", c);
-			exit(1);
-		}
-		writef("%s $%d, %%rax\n\t", op, read_number(cast(int)(c-'0')));
+Ast read_prim() {
+	dchar c;
+	if (!readf("%c", c)) {
+		stderr.writeln("Unexpected EOF");
+		exit(1);
 	}
+	else if (c.isNumber) {
+		return read_number(cast(int)(c - '0'));
+	}
+	else if (c == '"') {
+		return read_string();
+	}
+	stderr.writefln("Don't know how to handle '%c'", c);
+	exit(1);
+	return null;
 }
 
-void compile_expr(int n) {
-	n = read_number(n);
-	writef("\t.text\n\t"~
-		".global intfn\n"~
-		"intfn:\n\t"~
-		"mov $%d, %%eax\n\t", n);
-	compile_expr2();
+Ast read_expr2(Ast left) {
+	skip_space();
+	dchar c;
+	if (!readf("%c", c)) {
+		return left;
+	}
+	AST_TYPE op;
+	if (c == '+') {
+		op = AST_TYPE.OP_PLUS;
+	}
+	else if (c == '-') {
+		op = AST_TYPE.OP_MINUS;
+	}
+	else {
+		stderr.writefln("Operator expected, but got '%c'", c);
+		exit(1);
+	}
+	skip_space();
+	Ast right = read_prim();
+	return read_expr2(new Ast(op, left, right));
 }
 
-void compile_string() {
+Ast read_string() {
 	dchar[] buf;
 	while (true) {
 		dchar c;
@@ -88,35 +123,112 @@ void compile_string() {
 		buf ~= c;
 	}
 	buf ~= '\0';
-	writef("\t.data\n"~
+	return new Ast(buf.to!string);
+}
+
+Ast read_expr() {
+	Ast left = read_prim();
+	return read_expr2(left);
+}
+
+void print_quote(string s) {
+	foreach(c; s) {
+		if (c == '\"' || c == '\\') {
+			write("\\");
+		}
+		writef("%c", c);
+	}
+}
+void emit_string(Ast ast) {
+	write("\t.data\n"~
 		".mydata:\n\t"~
-		".string \"%s\"\n\t"~
+		".string \"");
+	print_quote(ast.sval);
+	write("\"\n\t"~
 		".text\n\t"~
 		".global stringfn\n"~
 		"stringfn:\n\t"~
-		"lea .mydata(%%rip), %%rax\n\t"~
-		"ret\n", buf.to!string);
-	exit(0);
+		"lea .mydata(%rip), %rax\n\t"~
+		"ret\n");
 }
-
-void compile() {
-	char c;
-	readf("%c", c);
-	if (c.isNumber) {
-		compile_expr(cast(int)(c-'0'));
+void emit_binop(Ast ast) {
+	string op;
+	if (ast.type == AST_TYPE.OP_PLUS) {
+		op = "add";
 	}
-	if (c == '"') {
-		compile_string();
+	else if (ast.type == AST_TYPE.OP_MINUS) {
+		op = "sub";
 	}
 	else {
-		stderr.writefln("Don't know how to handle '%c'", c);
+		stderr.writeln("invalid operand");
 		exit(1);
+	}
+	emit_intexpr(ast.left);
+	write("mov %eax, %ebx\n\t");
+	emit_intexpr(ast.right);
+	writef("%s %%ebx, %%eax\n\t", op);
+}
+void ensure_intexpr(Ast ast) {
+	if (ast.type != AST_TYPE.OP_PLUS &&
+		ast.type != AST_TYPE.OP_MINUS &&
+		ast.type != AST_TYPE.INT) {
+		stderr.writeln("integer or binary operator expected");
+		exit(1);
+	}
+}
+void emit_intexpr(Ast ast) {
+	ensure_intexpr(ast);
+	if (ast.type == AST_TYPE.INT) {
+		writef("mov $%d, %%eax\n\t", ast.ival);
+	}
+	else {
+		emit_binop(ast);
+	}
+}
+void print_ast(Ast ast) {
+	final switch(ast.type) {
+		case AST_TYPE.OP_PLUS:
+			write("(+ ");
+			goto print_op;
+		case AST_TYPE.OP_MINUS:
+			write("(- ");
+print_op:
+			print_ast(ast.left);
+			write(" ");
+			print_ast(ast.right);
+			write(")");
+			break;
+		case AST_TYPE.INT:
+			writef("%d", ast.ival);
+			break;
+		case AST_TYPE.STR:
+			print_quote(ast.sval);
+			break;
+	}
+}
+
+void compile(Ast ast) {
+	if (ast.type == AST_TYPE.STR) {
+		emit_string(ast);
+	}
+	else {
+		write(".text\n\t"~
+			".global intfn\n"~
+			"intfn:\n\t");
+		emit_intexpr(ast);
+		write("ret\n");
 	}
 }
 
 
 int main(string[] args)
 {
-	compile();
+	Ast ast = read_expr();
+	if (args.length > 1 && args[1] == "-a") {
+		print_ast(ast);
+	}
+	else {
+		compile(ast);
+	}
 	return 0;
 }
--- test.sh
+++ test.sh
@@ -12,16 +12,23 @@ function compile {
 		exit
 	fi
 }
-function test {
-	expected="$1"
-	expr="$2"
-
-	compile "$expr"
-	result="`./tmp.out`"
-	if [ "$result" != "$expected" ]; then
-		echo "Test failed: $expected expected but got $result"
+function assertequal {
+	if [ "$1" != "$2" ]; then
+		echo "Test failed: $2 expected but got $1"
 		exit
 	fi
+}
+function testast {
+	result="$(echo "$2" | ./8dcc -a)"
+	if [ $? -ne 0 ]; then
+		echo "Failed to compile $1"
+		exit
+	fi
+	assertequal "$result" "$1"
+}
+function test {
+	compile "$2"
+	assertequal "$(./tmp.out)" "$1"
 }
 function testfail {
 	expr="$1"
@@ -36,15 +43,20 @@ if [ $? -ne 0 ]; then
 	echo "dub build failed"
 	exit
 fi
+testast '1' '1'
+testast '(+ (- (+ 1 2) 3) 4)' '1+2-3+4'
+
 test 0 0
 test abc '"abc"'
 
 test 3 '1+2'
 test 3 '1 + 2'
 test 10 '1+2+3+4'
+test 4 '1+2-3+4'
 
 testfail '0abc'
 testfail '"abc'
 testfail '1+'
+testfail '1+"abc"'
 
 echo "All test passed"
